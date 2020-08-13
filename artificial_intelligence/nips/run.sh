d6a1832973f33423868d6e7a68762d5646fa1203
Shao Tao <shaotao@yahoo.com>
2020-08-13 23:50:46 +0800

    added nips

---
+++ artificial_intelligence/nips/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#time ./crawl_nips.py https://papers.nips.cc/book/advances-in-neural-information-processing-systems-32-2019 nips2019.csv
+
+time ./crawl_nips.py https://papers.nips.cc/book/advances-in-neural-information-processing-systems-31-2018 nips2018.csv
+
+
+time ./crawl_nips.py https://papers.nips.cc/book/advances-in-neural-information-processing-systems-30-2017 nips2017.csv
+
+
+time ./crawl_nips.py https://papers.nips.cc/book/advances-in-neural-information-processing-systems-29-2016 nips2016.csv
+
+
