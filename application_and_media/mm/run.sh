96aa6b0db456c9c70f8b0b10b209283da88d85dc
Shao Tao <shaotao@yahoo.com>
2020-08-14 00:50:36 +0800

    add acmmm2019

---
+++ application_and_media/mm/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ACM MM 2019
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading2 mm2019_multimodal_fusion_and_visual_relations.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading3 mm2019_affective_computing_and_facial_analytics.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading4 mm2019_fasion_and_human_analysis.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading5 mm2019_live_multimedia_application_and_streaming.csv
+#
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading7 mm2019_knowledge_processing_and_action_analysis.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading8 mm2019_adversarial_learning.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading9 mm2019_captioning_and_video_analysis.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading10 mm2019_3d_visual_processing.csv
+#
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading13 mm2019_best_paper.csv
+#
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading16 mm2019_multimodal_qa_and_content_generation.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading17 mm2019_attention_and_saliency.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading18 mm2019_smart_applications.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading19 mm2019_algorithms_in_multimedia.csv
+#
+#
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading22 mm2019_modal_retrieval.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading23 mm2019_visual_analysis_and_applications.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading24 mm2019_social_computing_and_image_processing.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading25 mm2019_embedding_and_network_learing.csv
+#
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading30 mm2019_summaries_and_generation.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading31 mm2019_quality_of_experience_and_interaction.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading32 mm2019_transport_and_delivery.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3343031?tocHeading=heading33 mm2019_art_and_culture.csv
+
+
+# ACM MM 2018
+
