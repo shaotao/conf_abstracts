b87e2b569e13e4e5b78835f044b2469549888799
Shao Tao <shaotao@yahoo.com>
2020-08-13 21:50:29 +0800

    done with kdd

---
+++ artificial_intelligence/kdd/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3292500?tocHeading=heading2 kdd2019_research.csv
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3292500?tocHeading=heading3 kdd2019_applied_data_science.csv
+
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3219819?tocHeading=heading2 kdd2018_applied_data_science1.csv
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3219819?tocHeading=heading3 kdd2018_applied_data_science2.csv
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3219819?tocHeading=heading4 kdd2018_research1.csv
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3219819?tocHeading=heading5 kdd2018_research2.csv
+
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3097983?tocHeading=heading4 kdd2017_research.csv
+#time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/3097983?tocHeading=heading6 kdd2017_applied_data_science.csv
+
+time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/2939672?tocHeading=heading3 kdd2016_applied_data_science.csv
+time ./crawl_kdd.py https://dl.acm.org/doi/proceedings/10.1145/2939672?tocHeading=heading6 kdd2016_research.csv
