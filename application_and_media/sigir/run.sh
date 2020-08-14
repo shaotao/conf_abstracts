fbda13df858c4638ff0d454db47e9a44820a9c6b
Shao Tao <shaotao@yahoo.com>
2020-08-15 02:35:23 +0800

    complete SIGIR 2016 to 2020

---
+++ application_and_media/sigir/run.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# ACM SIGIR 2020
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading2 sigir2020_1a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading3 sigir2020_1b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading4 sigir2020_1c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading5 sigir2020_2a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading6 sigir2020_2b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading7 sigir2020_2c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading8 sigir2020_3a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading9 sigir2020_3b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading10 sigir2020_3c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading11 sigir2020_4a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading12 sigir2020_4b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading13 sigir2020_4c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading14 sigir2020_5a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading15 sigir2020_5b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading16 sigir2020_5c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading17 sigir2020_6a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading18 sigir2020_6b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading19 sigir2020_6c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading20 sigir2020_7a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading21 sigir2020_7b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading22 sigir2020_7c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading23 sigir2020_8a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading24 sigir2020_8b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3397271?tocHeading=heading25 sigir2020_8c.csv
+
+# ACM SIGIR 2019
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading2 sigir2019_1a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading3 sigir2019_1b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading4 sigir2019_1c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading5 sigir2019_2a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading6 sigir2019_2b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading7 sigir2019_2c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading8 sigir2019_3a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading9 sigir2019_3b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading10 sigir2019_3c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading11 sigir2019_4a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading12 sigir2019_4b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading13 sigir2019_4c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading14 sigir2019_5a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading15 sigir2019_5b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading16 sigir2019_6a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading17 sigir2019_6b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading18 sigir2019_7a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading19 sigir2019_7b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading20 sigir2019_7c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading21 sigir2019_8a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading22 sigir2019_8b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading23 sigir2019_8c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading24 sigir2019_9a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading25 sigir2019_9b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3331184?tocHeading=heading26 sigir2019_9c.csv
+
+
+# ACM SIGIR 2018
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading2 sigir2018_1a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading3 sigir2018_1b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading4 sigir2018_1c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading5 sigir2018_1d.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading6 sigir2018_2a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading7 sigir2018_2b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading8 sigir2018_2c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading9 sigir2018_2d.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading10 sigir2018_3a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading11 sigir2018_3b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading12 sigir2018_3c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading13 sigir2018_3d.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading14 sigir2018_4a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading15 sigir2018_4b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading16 sigir2018_4c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading17 sigir2018_4d.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading18 sigir2018_5a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading19 sigir2018_5b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading20 sigir2018_5c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading21 sigir2018_5d.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading22 sigir2018_6a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading23 sigir2018_6b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading24 sigir2018_6c.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading25 sigir2018_6d.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading26 sigir2018_7a.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading27 sigir2018_7b.csv
+#time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3209978?tocHeading=heading28 sigir2018_7c.csv
+
+# ACM SIGIR 2017
+#n=0
+#for i in {3..21};do
+#    n=$((n+1))
+#    time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/3077136?tocHeading=heading${i} sigir2017_${n}.csv
+#done
+
+
+# ACM SIGIR 2016
+n=0
+for i in {3..26};do
+    n=$((n+1))
+    time ./crawl_sigir.py https://dl.acm.org/doi/proceedings/10.1145/2911451?tocHeading=heading${i} sigir2016_${n}.csv
+done
