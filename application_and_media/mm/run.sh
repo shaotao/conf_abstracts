3dccc98032f3e6d46808fe8e9e168606a69de503
Shao Tao <shaotao@yahoo.com>
2020-08-15 01:26:11 +0800

    completed ACM MM 2016 to 2019.

--- application_and_media/mm/run.sh
+++ application_and_media/mm/run.sh
@@ -31,4 +31,69 @@
 
 
 # ACM MM 2018
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading1 mm2018_ff1.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading3 mm2018_ff2.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading5 mm2018_mm_image_translation.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading6 mm2018_machine_learning.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading7 mm2018_recommendation.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading8 mm2018_object_scene_understanding.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading9 mm2018_multimodal_reasoning.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading10 mm2018_video_analysis_and_streaming.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading11 mm2018_ff3.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading13 mm2018_best_paper.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading15 mm2018_ff4.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading17 mm2018_recognition.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading18 mm2018_social_emotional_mm.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading21 mm2018_application_in_mm.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading22 mm2018_cross_modal_translation.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading24 mm2018_ff5.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading26 mm2018_mm_search.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading27 mm2018_mm_entertainment_and_experience.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading28 mm2018_smart_mm_system.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading29 mm2018_ff6.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading31 mm2018_image_processing.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading32 mm2018_brave_new_ideas.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3240508?tocHeading=heading34 mm2018_representation_learning.csv
+
+
+# ACM MM 2017
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading1 mm2017_ff1.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading3 mm2017_best_paper.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading4 mm2017_ff2.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading5 mm2017_deep_learning_for_mm1.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading7 mm2017_social_and_affective_mm.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading8 mm2017_system_and_applications.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading9 mm2017_mm_search_and_recommendation.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading12 mm2017_ff3.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading16 mm2017_ff4.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading18 mm2017_deep_learning_for_mm2.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading19 mm2017_video_streaming.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading20 mm2017_perception_affect_and_interaction.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading22 mm2017_mm_art.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading25 mm2017_video.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading26 mm2017_ff5.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading27 mm2017_deep_learning_for_mm3.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading28 mm2017_novel1.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading29 mm2017_ff6.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading31 mm2017_multimodal_analysis_and_description.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading32 mm2017_brave_new_ideas.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading33 mm2017_understanding.csv
+#time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/3123266?tocHeading=heading35 mm2017_novel2.csv
+
+
+# ACM MM 2016
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading2 mm2016_best_paper.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading7 mm2016_topics_in_mm1.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading8 mm2016_analysis_and_search.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading9 mm2016_video_analysis_and_streaming.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading10 mm2016_topics_in_mm2.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading11 mm2016_brave_new_topic.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading12 mm2016_deep_learning.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading13 mm2016_events_and_context.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading16 mm2016_topics_in_mm3.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading18 mm2016_learning_and_hashing.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading19 mm2016_transport_and_experience.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading20 mm2016_topics_in_mm4.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading21 mm2016_analysis_and_middleware.csv
+time ./crawl_mm.py https://dl.acm.org/doi/proceedings/10.1145/2964284?tocHeading=heading22 mm2016_emotion_people_and_faces.csv
 
